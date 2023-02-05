bc60f98ac441f0ddd47cd63b3212c4762a2e53ec
Rajesh25B <basvoju.rajesh@gmail.com>
2023-02-05 13:43:06 +0530

    build sidebar and its icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,66 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import BalanceIcon from "@mui/icons-material/Balance";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
+import Switch from "@mui/material/Switch";
 
 export function Sidebar() {
   return (
     <Box
-      sx={{ backgroundColor: "#4295b3", display: { xs: "none", sm: "block" } }}
+      sx={{ display: { xs: "none", sm: "block" } }}
       flex={1}
       p={1}
+      paddingTop={8}
     >
-      Sidebar
+      {/* MUI  Lists */}
+
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItemButton>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <AnalyticsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Analytics" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <BarChartIcon />
+          </ListItemIcon>
+          <ListItemText primary="Sales" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <BalanceIcon />
+          </ListItemIcon>
+          <ListItemText primary="Worksheets" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <AccountCircleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Profile" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <NightsStayIcon />
+          </ListItemIcon>
+          <Switch />
+        </ListItemButton>
+      </List>
     </Box>
   );
 }
